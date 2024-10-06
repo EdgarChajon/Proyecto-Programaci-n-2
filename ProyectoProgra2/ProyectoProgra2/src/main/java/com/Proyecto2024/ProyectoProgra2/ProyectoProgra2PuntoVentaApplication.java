95fca11848827106cd8e4f5710973f1865b1796d
EdgarChajon <romariochajon10@gmail.com>
2024-10-06 16:48:18 -0600

    Se inicia el proyecto
    se descargaron todas las dependencias

---
+++ ProyectoProgra2/ProyectoProgra2/src/main/java/com/Proyecto2024/ProyectoProgra2/ProyectoProgra2PuntoVentaApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto2024.ProyectoProgra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProgra2PuntoVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoProgra2PuntoVentaApplication.class, args);
+	}
+
+}
---
+++ ProyectoProgra2/ProyectoProgra2/src/test/java/com/Proyecto2024/ProyectoProgra2/ProyectoProgra2PuntoVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto2024.ProyectoProgra2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoProgra2PuntoVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
